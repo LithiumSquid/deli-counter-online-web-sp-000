1a731ae52613e96f1b8dc4e188a758c7a5eabbe8
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-11 14:39:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, name = "Ashlyn")
 end
   
 def now_serving(katz_deli)
-  if katz_deli.length? >= 1 
+  if katz_deli.length >= 1 
     puts "Currently serving " + katz_deli.shift + "."
   else
     puts "There is nobody waiting to be served!"
