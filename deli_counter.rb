bd3afd860c86bc189358ac8e5fee88db589b4c31
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-11 14:19:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli, index)
+  if katz_deli.length >= 1
+    puts "The line is currently:" + katz_deli 
+  else if katz_deli.length = 0
+    puts "The line is currently empty."
+  else
+    nil
+end
+end
+end
+
+def take_a_number(katz_deli, name = "Ashlyn")
+  katz_deli << name
+  position = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+  
+def now_serving(katz_deli)
+  if katz_deli.lenth >= 1 
+    puts "Currently serving" + katz_deli.shift + "."
+    line(katz_deli)
+  else
+    puts "There is nobody waiting to be served!"
+end
+end
